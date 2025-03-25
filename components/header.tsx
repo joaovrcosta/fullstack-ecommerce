1cf058c29c0d2b45ffa1f8658c7cd7fbe2f1a7aa
joaovrcosta <joaovr.costa@gmail.com>
2025-03-25 15:20:18 -0300

    first commit

---
+++ components/header.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import {
+  ClerkLoaded,
+  SignedIn,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+import Link from "next/link";
+import Form from "next/form";
+import React from "react";
+import { PackageIcon } from "@sanity/icons";
+import { Button } from "./ui/button";
+
+function Header() {
+  const { user } = useUser();
+
+  const createClerkPasskey = async () => {
+    try {
+      const response = await user?.createPasskey();
+      console.log(response);
+    } catch (err) {
+      console.log("errro", JSON.stringify(err, null, 2));
+    }
+  };
+
+  return (
+    <header className="flex flex-wrap justify-between items-center px-4 py-2 w-full">
+      <div className="w-full flex flex-wrap justify-between items-center">
+        <Link
+          href="/"
+          className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
+        >
+          Shop
+        </Link>
+        <Form
+          action="/search"
+          className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
+        >
+          <input
+            type="text"
+            name="query"
+            placeholder="Search for products"
+            className="bg-gray-100 text-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border w-full max-w-4xl"
+          />
+        </Form>
+        <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
+          <Link
+            href="/cart"
+            className="flex bg-blue-400 px-4 py-2 rounded w-full items-center justify-center"
+          >
+            <span>My Cart</span>
+          </Link>
+
+          <ClerkLoaded>
+            <SignedIn>
+              <Link
+                href="/orders"
+                className="flex bg-blue-400 px-4 py-2 rounded w-full justify-center items-center"
+              >
+                <div className="space-x-2 flex items-center">
+                  <PackageIcon className="h-6 w-6" />
+                  <span>Orders</span>
+                </div>
+              </Link>
+            </SignedIn>
+
+            {user ? (
+              <div className="flex items-center space-x-2">
+                <UserButton />
+                <div className="hidden sm:block text-xs">
+                  <p className="text-gray-400">Welcome back</p>
+                  <p className="text-gray-800">{user.fullName}!</p>
+                </div>
+              </div>
+            ) : (
+              <SignInButton />
+            )}
+
+            {user?.passkeys.length === 0 && (
+              <Button
+                onClick={createClerkPasskey}
+                className="bg-white hover:bg-blue-700 hover:text-white animate-pulse text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
+              >
+                Create passkey
+              </Button>
+            )}
+          </ClerkLoaded>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
